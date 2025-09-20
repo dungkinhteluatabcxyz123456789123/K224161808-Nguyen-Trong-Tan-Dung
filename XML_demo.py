a97eef065ab595499fcd0d72c5c92accbc1c57f8
dungkinhteluatabcxyz123456789123 <dungntt22416c@st.uel.edu.vn>
2025-09-20 11:35:02 +0700

    Delete XML_demo.py

--- XML_demo.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-# Reading the data inside the ml file to a variable under the name data
-with open('../Data set/SalesTransactions.xml', 'r') as f:
-    data = f.read()
-# Passing the stored data inside the beautifulsoup parser
-bs_data = BeautifulSoup(data, 'xml')
-# Finding all instances of tag
-UelSample = bs_data. find_all( 'UelSample')
-print (UelSample)

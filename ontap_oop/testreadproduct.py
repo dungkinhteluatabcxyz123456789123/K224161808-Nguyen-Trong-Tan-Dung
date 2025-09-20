870e28e5c9e8ab33ec0a833ded8abc99c65e56b8
dungkinhteluatabcxyz123456789123 <dungntt22416c@st.uel.edu.vn>
2025-09-20 11:31:12 +0700

    Delete ontap_oop directory

--- ontap_oop/testreadproduct.py
+++
@@ -1,8 +0,0 @@
-from K224161808.ontap_oop.filefactory import FileFactory
-from K224161808.ontap_oop.product import Product
-
-ff=FileFactory
-dataset=ff.readData("mydataset.json",Product)
-print("Danh sach san pham")
-for product in dataset:
-    print(product)
